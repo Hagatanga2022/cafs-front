0f3755809f69f0924332825c15fc3ac85f964f5b
Paulo <paulorolinski.ifc@gmail.com>
2022-09-06 00:11:43 -0300

    Login 1.0

---
+++ src/components/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-btn @click="login = true" class="v-btn1" v-on="on">Login</v-btn>
+    <v-dialog v-model="login" max-width="600px" max-height="600px">
+      <v-card class="pop-up-card">
+        <v-card-title class="text-h3 justify-center">Login</v-card-title>
+        <v-col>
+          <v-text-field
+            class="input"
+            square
+            outlined
+            v-model="text"
+            label="Digite seu e-mail"
+          ></v-text-field>
+          <v-text-field
+            class="input"
+            square
+            outlined
+            v-model="text"
+            label="Digite sua senha"
+          >
+          </v-text-field>
+        </v-col>
+      </v-card>
+    </v-dialog>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      login: false,
+    };
+  },
+};
+</script>
+
+<style>
+.pop-up-card {
+    justify-items: center;
+}
+.input {
+  max-width: 400px;
+  display: block;
+  margin: 0 auto;
+}
+.v-btn1:before {
+  content: "";
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  border-radius: inherit;
+  box-shadow: none;
+}
+</style>
--- src/pages/StartPage.vue
+++ src/pages/StartPage.vue
@@ -1,31 +1,16 @@
 <template>
-  <v-app  style="background-color:#1D6382" view="lHh Lpr lFf">
-    <v-app-bar
-    flat
-    outlined
-    color="#1D6382"
-    >
-    <div class="btn-container">
-      <div >
-        <v-btn class="v-btn btn-text">
-         Cadastro
-        </v-btn>
-      </div>
-      <v-divider 
-      dark
-      vertical
-      inset
-      class="mx-4"
-      >
-      </v-divider>
-      <div>
-        <v-btn class="v-btn">
-         Login
-        </v-btn>
-      </div>
-    </div>
-      <div class="spheres"
-      >
+  <v-app style="background-color: #1d6382" view="lHh Lpr lFf">
+    <v-app-bar flat outlined color="#1D6382">
+        <div class="btn-container">
+          <div>
+            <v-btn class="v-btn btn-text"> Cadastro </v-btn>
+          </div>
+          <v-divider dark vertical inset class="mx-4"> </v-divider>
+          <div>
+            <Login />
+          </div>
+        </div>
+      <div class="spheres">
         <div class="sphere"></div>
         <div class="sphere"></div>
         <div class="sphere">
@@ -34,64 +19,60 @@
         <div class="sphere"></div>
         <div class="sphere"></div>
         <div class="sphere"></div>
-
       </div>
     </v-app-bar>
     <v-container>
-      <div 
-      style="
-      background-color:#1D6382;
-      padding-top:10%;
-  
-
-      "
-      class="text-container "
+      <div
+        style="background-color: #1d6382; padding-top: 10%"
+        class="text-container"
       >
-      <div class="sphere-cube"></div>
-      <div class="cube"></div>
-      <div class="cube">
-        <div class="image1"></div>
-      </div>
-      <div class="text">
-        <div class="text text-shadow">
-          Controle de Acesso a Fábrica de Software
+        <div class="sphere-cube"></div>
+        <div class="cube"></div>
+        <div class="cube">
+          <div class="image1"></div>
         </div>
-        <div class="text text-shadow">
-          O Controle de acesso 
-          à fábrica de software, 
-          referido pelo acrônimo C.A.F.S, 
-          é um sistema que busca auxiliar 
-          os coordenadores e as suas equipes
-          a gerenciarem a sua jornada de trabalho 
-          previamente estabelecida
+        <div class="text">
+          <div class="text text-shadow">
+            Controle de Acesso a Fábrica de Software
+          </div>
+          <div class="text text-shadow">
+            O Controle de acesso à fábrica de software, referido pelo acrônimo
+            C.A.F.S, é um sistema que busca auxiliar os coordenadores e as suas
+            equipes a gerenciarem a sua jornada de trabalho previamente
+            estabelecida
+          </div>
         </div>
       </div>
-    </div>
     </v-container>
   </v-app>
 </template>
 
 <script>
-
+import Login from "../components/Login.vue";
+export default {
+  components: {
+    Login,
+  },
+};
 </script>
+
 <style>
 @viewport {
   user-zoom: fixed;
 }
-.sidebar-content{
+.sidebar-content {
   height: 100%;
 }
-.title{
+.title {
   color: white;
   font-size: 40px;
   font-weight: 500;
 }
-.minor-title{
+.minor-title {
   color: white;
 }
-.container{
+.container {
   height: 93vh;
-  
 }
 .btn-container{
   display: flex;
@@ -99,48 +80,35 @@
   width: 100%;
   justify-content: right;
   padding: 10px;
-  
 }
-.v-btn:before {
-    content: "";
-    display: block;
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    border-radius: inherit;
-    box-shadow:none;
-}
-
-.cube:nth-child(3){
-  background-color: #FFFFFF;
+.cube:nth-child(3) {
+  background-color: #ffffff;
   width: 36vw;
   height: 70vh;
   border-radius: 30px;
   transform: rotate(45deg);
   position: fixed;
   left: 12vw;
-  top:50vh;
-  display:flex;
+  top: 50vh;
+  display: flex;
   box-shadow: 0px 10px 30px #3b3b3b57;
   border-right: rgba(0, 0, 0, 0.397) 1px solid;
-  overflow:hidden;
+  overflow: hidden;
   justify-content: center;
   align-content: center;
   align-items: center;
 }
-.cube:nth-child(3) .image1{
+.cube:nth-child(3) .image1 {
   background-image: url(../assets/AA1.png);
-  width:60%;
+  width: 60%;
   height: 75%;
-  position:fixed;
-  background-size:contain;
+  position: fixed;
+  background-size: contain;
   border-radius: 200px 200px 100px 100px;
   transform: rotate(315deg);
 }
-.cube:nth-child(2){
-  background-color: #D9D9D9;
+.cube:nth-child(2) {
+  background-color: #d9d9d9;
   width: 37vw;
   height: 70vh;
   border-radius: 40px;
@@ -150,115 +118,114 @@
   top: 45vh;
   box-shadow: 0px 10px 30px #3b3b3b57;
 }
-.sphere-cube{
-  background-color: #5A8A9F; 
+.sphere-cube {
+  background-color: #5a8a9f;
   width: 9vw;
   height: 9vw;
   border-radius: 100px;
-  position:relative;
+  position: relative;
   left: -25vw;
   top: 5vh;
 }
-.sphere:nth-child(6){
-  background-color: #5A8A9F; 
+.sphere:nth-child(6) {
+  background-color: #5a8a9f;
   width: 9vw;
   height: 9vw;
   border-radius: 100px;
-  position:relative;
+  position: relative;
   right: 10vw;
   top: 67vh;
 }
-.sphere:nth-child(5){
-  background-color: #5a8a9f81; 
+.sphere:nth-child(5) {
+  background-color: #5a8a9f81;
   width: 6vw;
   height: 6vw;
   border-radius: 100px;
-  position:relative;
+  position: relative;
   right: 8vw;
-  top:73vh;
+  top: 73vh;
 }
-.sphere:nth-child(4){
-  background-color: #5a8a9fb6; 
+.sphere:nth-child(4) {
+  background-color: #5a8a9fb6;
   width: 6vw;
   height: 6vw;
   border-radius: 100px;
-  position:fixed;
+  position: fixed;
   left: 350px;
   top: 65px;
 }
-.sphere:nth-child(3){
-  background-color: #6e9eb2
-  ; 
+.sphere:nth-child(3) {
+  background-color: #6e9eb2;
   width: 9vw;
   height: 9vw;
   border-radius: 100px;
-  position:absolute;
+  position: absolute;
   left: 3.7vw;
   top: 5vh;
   overflow: hidden;
   display: flex;
   padding-top: 1%;
   justify-content: space-around;
-  align-content:space-around;
+  align-content: space-around;
 }
-.sphere:nth-child(3) .image{
+.sphere:nth-child(3) .image {
   background-image: url(../assets/IMG-20220617-WA0011.jpg);
   width: 60%;
   height: auto;
   background-size: contain;
   border-radius: 10px;
 }
-.sphere:nth-child(2){
-  background-color: #5a8a9f9f; 
+.sphere:nth-child(2) {
+  background-color: #5a8a9f9f;
   width: 6vw;
   height: 6vw;
   border-radius: 100px;
-  position:absolute;
+  position: absolute;
   left: 4vh;
   top: 11vh;
 }
-.sphere:nth-child(1){
-  background-color: #5a8a9f81; 
+.sphere:nth-child(1) {
+  background-color: #5a8a9f81;
   width: 6vw;
   height: 6vw;
   border-radius: 100px;
-  position:absolute;
+  position: absolute;
   left: 9vw;
   top: 6vh;
 }
-.spheres{
+.spheres {
   width: 0;
   height: 0;
 }
-.text{
+.text {
   width: 60vh;
   height: 26vh;
-  background-color:#1d6482ea;
+  background-color: #1d6482ea;
   border-radius: 20px 0px 10px 20px;
 }
-.text:nth-child(1){
+.text:nth-child(1) {
   font-size: 3em;
   line-height: 100%;
-  text-align:right;
+  text-align: right;
   position: absolute;
   top: 30vh;
   right: 10vh;
 }
-.text:nth-child(2){
+.text:nth-child(2) {
   font-size: 1.75em;
   line-height: 100%;
-  text-align:right;
+  text-align: right;
   position: absolute;
   right: 10vh;
   top: 50vh;
 }
-html{
+html {
   overflow: hidden;
 }
-.text{
+.text {
   color: white;
 }
-.text-container{
+.text-container {
   height: 100%;
   width: 100%;
   justify-content: flex-end;
