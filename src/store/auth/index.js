8d7c48863a661d0062b852ef562d8eabf812b0b2
Ricardo André <ricardo.andre.ifc@gmail.com>
2022-10-27 23:22:53 -0300

    removendo Headers desnecesários

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -18,15 +18,9 @@ export const auth = {
     },
     setHeaders(state) {
       axios.defaults.headers.common["Authorization"] = `Bearer ${state.token}`;
-      axios.defaults.headers.get["Authorization"] = `Bearer ${state.token}`;
-      axios.defaults.headers.post["Authorization"] = `Bearer ${state.token}`;
-      axios.defaults.headers.patch["Authorization"] = `Bearer ${state.token}`;
     },
     unsetHeaders() {
       delete axios.defaults.headers.common["Authorization"];
-      delete axios.defaults.headers.get["Authorization"];
-      delete axios.defaults.headers.post["Authorization"];
-      delete axios.defaults.headers.patch["Authorization"];
     },
     setLogout(state) {
       state.user = null;
