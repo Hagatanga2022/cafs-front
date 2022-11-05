e853bb4f60d21dafdde29113ea224e5bda561cd6
Gabriel Gomes Galikosky <gomesgalikosky@gmail.com>
2022-11-05 20:38:58 -0300

    Reestilização cadastro

--- src/components/Cadastro.vue
+++ src/components/Cadastro.vue
@@ -2,84 +2,97 @@
   <v-container>
     <v-btn @click="newAccount = true" class="v-btn1">Cadastro</v-btn>
     <v-dialog v-model="newAccount" max-width="600px" max-height="600px">
-      <v-card class="pop-up-card" elevation="10" outlined>
-        <v-card-title class="text-h3 mb-5 justify-center teal--text"
-          >Cadastro</v-card-title
-        >
-        <v-col>
-          <v-text-field
-            class="input mx-auto"
-            @keyup.enter="submitForm"
-            color="teal"
-            square
-            outlined
-            v-model="form.username"
-            label="Apelido"
+      <v-card class="mx-auto" max-width="600">
+        <v-card-title class="text-h6 font-weight-regular justify-space-between">
+          <span>{{ currentTitle }}</span>
+          <v-avatar
+            color="primary lighten-2"
+            class="subheading white--text"
+            size="24"
+            v-text="step"
+          ></v-avatar>
+        </v-card-title>
+
+        <v-window v-model="step">
+          <v-window-item :value="1">
+            <v-card-text>
+              <v-text-field
+                label="Apelido"
+                @keyup.enter="submitForm"
+              ></v-text-field>
+
+              <v-text-field
+                label="Email"
+                value="youremail@gmail.com"
+                append-icon="mdi-email"
+              ></v-text-field>
+
+              <span class="text-caption grey--text text--darken-1">
+                Este é o e-mail que você usará para fazer login na sua conta
+                C.A.F.S
+              </span>
+            </v-card-text>
+          </v-window-item>
+
+          <v-window-item :value="2">
+            <v-card-text>
+              <v-text-field label="SIAPE" type="number"></v-text-field>
+
+              <v-text-field label="CPF" type="number"></v-text-field>
+
+              <v-text-field
+                label="Senha"
+                :type="show ? 'text' : 'password'"
+                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="show = !show"
+                @keyup.enter="submitForm"
+              >
+              </v-text-field>
+
+              <v-text-field
+                label="Confirme sua senha"
+                :type="show ? 'text' : 'password'"
+                @click:append="show = !show"
+                @keyup.enter="submitForm"
+              ></v-text-field>
+              <span class="text-caption grey--text text--darken-1">
+                Por favor, digite uma senha para sua a conta
+              </span>
+            </v-card-text>
+          </v-window-item>
+
+          <v-window-item :value="3">
+            <div class="pa-4 text-center">
+              <v-img
+                class="mb-4"
+                contain
+                height="128"
+                src="../assets/CAFS1.png"
+              ></v-img>
+              <h3 class="text-h6 font-weight-light mb-2">
+                Bem vindo ao C.A.F.S.
+              </h3>
+              <span class="text-caption grey--text"
+                > Obrigado por se cadastrar!</span
+              >
+            </div>
+          </v-window-item>
+        </v-window>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-btn :disabled="step === 1" text @click="step--"> Voltar </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn
+            :disabled="step === 3"
+            color="primary"
+            depressed
+            @click="step++"
           >
-          </v-text-field>
-          <v-text-field
-            class="input mx-auto"
-            @keyup.enter="submitForm"
-            color="teal"
-            append-icon="mdi-email"
-            square
-            outlined
-            v-model="form.email"
-            label="Email"
-            hint="www.example.com/page"
-          >
-          </v-text-field>
-          <v-text-field
-            class="input mx-auto"
-            @keyup.enter="submitForm"
-            color="teal"
-            type="number"
-            square
-            outlined
-            v-model="form.siape"
-            label="Siape"
-          >
-          </v-text-field>
-          <v-text-field
-            class="input mx-auto"
-            @keyup.enter="submitForm"
-            color="teal"
-            type="number"
-            square
-            outlined
-            v-model="form.cpf"
-            label="CPF"
-          >
-          </v-text-field>
-          <v-text-field
-            class="input mx-auto"
-            @keyup.enter="submitForm"
-            color="teal"
-            :type="show ? 'text' : 'password'"
-            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="show = !show"
-            outlined
-            v-model="form.password1"
-            label="Senha"
-          >
-          </v-text-field>
-          <v-text-field
-            class="input mx-auto"
-            @keyup.enter="submitForm"
-            color="teal"
-            :type="show ? 'text' : 'password'"
-            @click:append="show = !show"
-            outlined
-            v-model="form.password2"
-            label="Confirme sua senha"
-          >
-          </v-text-field>
-          <div align="center">
-            <v-btn class="ma-1 white--text" @click="submitForm" color="teal"
-              >Cadastrar</v-btn
-            >
-          </div>
-        </v-col>
+            Próximo
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-dialog>
     <v-snackbar
@@ -104,6 +117,7 @@ export default {
       errorMessage: null,
       errorRegister: false,
       form: {},
+      step: 1,
     };
   },
   methods: {
@@ -125,29 +139,17 @@ export default {
       }
     },
   },
+  computed: {
+    currentTitle() {
+      switch (this.step) {
+        case 1:
+          return "Cadastrar-se";
+        case 2:
+          return "Criar senha";
+        default:
+          return "Conta criada";
+      }
+    },
+  },
 };
 </script>
-
-<style>
-.pop-up-card {
-  justify-items: center;
-  border-radius: 100px;
-}
-
-.input {
-  max-width: 400px;
-  display: block;
-}
-
-.v-btn1:before {
-  content: "";
-  display: block;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  border-radius: inherit;
-  box-shadow: none;
-}
-</style>
--- src/pages/Bolsistas.vue
+++ src/pages/Bolsistas.vue
@@ -1,38 +1,141 @@
 <template>
   <v-app>
-    <v-card id="content">
-      <v-toolbar
-        class="white--text d-flex"
-        color="#1d6382"
-        dark
-        extended
-        extension-height="100"
-        elevation="1"
-      >
-        <v-img
-          id="Bolsista"
-          alt="Bolsista"
-          :src="Bolsista"
-          width="200"
-          height="200"
-        ></v-img>
-        <v-spacer></v-spacer>
-        <v-toolbar-title class="title ml-5">
-          <h1>Bolsistas</h1>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-      </v-toolbar>
+    <v-card>
+      <v-card-title class="indigo white--text text-h5">
+        Diretório de bolsitas
+      </v-card-title>
+      <v-row class="pa-4" justify="space-between">
+        <v-col cols="5">
+          <v-treeview
+            :active.sync="active"
+            :items="items"
+            :load-children="fetchUsers"
+            :open.sync="open"
+            activatable
+            color="warning"
+            open-on-click
+            transition
+          >
+            <template v-slot:prepend="{ item }">
+              <v-icon v-if="!item.children"> mdi-account </v-icon>
+            </template>
+          </v-treeview>
+        </v-col>
+
+        <v-divider vertical></v-divider>
+
+        <v-col class="d-flex text-center">
+          <v-scroll-y-transition mode="out-in">
+            <div
+              v-if="!selected"
+              class="text-h6 grey--text text--lighten-1 font-weight-light"
+              style="align-self: center"
+            >
+              Selecione o bolsista
+            </div>
+            <v-card
+              v-else
+              :key="selected.id"
+              class="pt-6 mx-auto"
+              flat
+              max-width="400"
+            >
+              <v-card-text>
+                <v-avatar v-if="avatar" size="88">
+                  <v-img
+                    :src="`https://avataaars.io/${avatar}`"
+                    class="mb-6"
+                  ></v-img>
+                </v-avatar>
+                <h3 class="text-h5 mb-2">
+                  {{ selected.name }}
+                </h3>
+                <div class="blue--text mb-2">
+                  {{ selected.email }}
+                </div>
+                <div class="blue--text subheading font-weight-bold">
+                  {{ selected.username }}
+                </div>
+              </v-card-text>
+              <v-divider></v-divider>
+              <v-row class="text-left" tag="v-card-text">
+                <v-col class="text-right mr-4 mb-2" tag="strong" cols="5">
+                  Company:
+                </v-col>
+                <v-col>{{ selected.company.name }}</v-col>
+                <v-col class="text-right mr-4 mb-2" tag="strong" cols="5">
+                  E-mail:
+                </v-col>
+                <v-col>
+                  <a :href="`//${selected.email}`" target="_blank">{{
+                    selected.email
+                  }}</a>
+                </v-col>
+                <v-col class="text-right mr-4 mb-2" tag="strong" cols="5">
+                  Telefone:
+                </v-col>
+                <v-col>{{ selected.phone }}</v-col>
+              </v-row>
+            </v-card>
+          </v-scroll-y-transition>
+        </v-col>
+      </v-row>
     </v-card>
   </v-app>
 </template>
 
 <script>
+const avatars = [
+  "?accessoriesType=Blank&avatarStyle=Circle&clotheColor=PastelGreen&clotheType=ShirtScoopNeck&eyeType=Wink&eyebrowType=UnibrowNatural&facialHairColor=Black&facialHairType=MoustacheMagnum&hairColor=Platinum&mouthType=Concerned&skinColor=Tanned&topType=Turban",
+  "?accessoriesType=Sunglasses&avatarStyle=Circle&clotheColor=Gray02&clotheType=ShirtScoopNeck&eyeType=EyeRoll&eyebrowType=RaisedExcited&facialHairColor=Red&facialHairType=BeardMagestic&hairColor=Red&hatColor=White&mouthType=Twinkle&skinColor=DarkBrown&topType=LongHairBun",
+  "?accessoriesType=Prescription02&avatarStyle=Circle&clotheColor=Black&clotheType=ShirtVNeck&eyeType=Surprised&eyebrowType=Angry&facialHairColor=Blonde&facialHairType=Blank&hairColor=Blonde&hatColor=PastelOrange&mouthType=Smile&skinColor=Black&topType=LongHairNotTooLong",
+  "?accessoriesType=Round&avatarStyle=Circle&clotheColor=PastelOrange&clotheType=Overall&eyeType=Close&eyebrowType=AngryNatural&facialHairColor=Blonde&facialHairType=Blank&graphicType=Pizza&hairColor=Black&hatColor=PastelBlue&mouthType=Serious&skinColor=Light&topType=LongHairBigHair",
+  "?accessoriesType=Kurt&avatarStyle=Circle&clotheColor=Gray01&clotheType=BlazerShirt&eyeType=Surprised&eyebrowType=Default&facialHairColor=Red&facialHairType=Blank&graphicType=Selena&hairColor=Red&hatColor=Blue02&mouthType=Twinkle&skinColor=Pale&topType=LongHairCurly",
+];
+const pause = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 import Bolsista from "../assets/bolsista.png";
 export default {
   data() {
     return {
       Bolsista,
+      active: [],
+      avatar: null,
+      open: [],
+      users: [],
     };
+  },
+  computed: {
+    items() {
+      return [
+        {
+          name: "Bolsistas",
+          children: this.users,
+        },
+      ];
+    },
+    selected() {
+      if (!this.active.length) return undefined;
+      const id = this.active[0];
+      return this.users.find((user) => user.id === id);
+    },
+  },
+  watch: {
+    selected: "randomAvatar",
+  },
+  methods: {
+    async fetchUsers(item) {
+      // Remove in 6 months and say
+      // you've made optimizations! :)
+      await pause(1500);
+      return fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => res.json())
+        .then((json) => item.children.push(...json))
+        .catch((err) => console.warn(err));
+    },
+    randomAvatar() {
+      this.avatar = avatars[Math.floor(Math.random() * avatars.length)];
+    },
   },
 };
 </script>
