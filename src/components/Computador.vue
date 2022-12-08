3a350f6b0ee3dbf66ca634fb553acede8a5dddbb
Ricardo André <ricardo.andre.ifc@gmail.com>
2022-12-07 21:21:53 -0300

    criação de computadores e listagem

--- src/components/Computador.vue
+++ src/components/Computador.vue
@@ -1,98 +1,126 @@
 <template>
-    <v-container>
-      <v-btn @click="loginPopUp = true" class="v-btn1">Cadastrar um computador</v-btn>
-      <v-dialog v-model="loginPopUp" max-width="600px" max-height="600px">
-        <v-card class="pop-up-card" elevation="10" outlined>
-          <v-form ref="form" v-model="valid" lazy-validation>
-            <v-card-title
-              class="text-h6 font-weight-regular justify-space-between"
+  <v-container>
+    <v-btn @click="loginPopUp = true" class="v-btn1"
+      >Cadastrar um computador</v-btn
+    >
+    <v-dialog v-model="loginPopUp" max-width="600px" max-height="600px">
+      <v-card class="pop-up-card" elevation="10" outlined>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-card-title
+            class="text-h6 font-weight-regular justify-space-between"
+          >
+            Adicionar um computador
+          </v-card-title>
+
+          <v-col justify="center">
+            <v-text-field
+              @keyup.enter="postComputerInfo"
+              color="teal"
+              square
+              required
+              v-model="computer.name"
+              label="Nome"
+            ></v-text-field>
+
+            <v-text-field
+              @keyup.enter="postComputerInfo"
+              color="teal"
+              square
+              required
+              v-model="computer.locationX"
+              label="Fileira"
             >
-              Adicionar um computador
-            </v-card-title>
-  
-            <v-col justify="center">
-              <v-text-field
-                @keyup.enter="submitLogin"
-                color="teal"
-                
-                square
-                required
-                v-model="user.username"
-                label="Nome"
-              ></v-text-field>
-  
-              <v-text-field
-                @keyup.enter="submitLogin"
-                color="teal"
-                
-               
-                square
-                required
-                v-model="user.password"
-                label="Fileira"
+            </v-text-field>
+            <v-text-field
+              @keyup.enter="postComputerInfo"
+              color="teal"
+              square
+              required
+              v-model="computer.locationY"
+              label="Coluna"
+            >
+            </v-text-field>
+            <v-file-input
+              :rules="rules"
+              square
+              outlined
+              type="file"
+              ref="file"
+              @change="uploadFile"
+              show-size
+              accept="image/png, image/jpeg, image/bmp"
+              placeholder="Upe sua foto de perfil"
+              prepend-icon=""
+              prepend-inner-icon="mdi-camera"
+              label="Foto do computador"
+            ></v-file-input>
+
+            <div class="mt-2" align="center">
+              <v-btn
+                :disabled="!valid"
+                class="mt-1 white--text"
+                color="blue"
+                @click="postComputerInfo"
+                >Adicionar</v-btn
               >
-              </v-text-field>
-              <v-text-field
-                
-                color="teal"
-                
-                
-                
-                square
-                required
-                v-model="user.password"
-                label="Número"
-              >
-              </v-text-field>
-              
-  
-              <div class="mt-2" align="center">
-                <v-btn
-                  
-                  :disabled="!valid"
-                  class="mt-1 white--text"
-                  color="blue"
-                  >Adicionar</v-btn
-                >
-              </div>
-            </v-col>
-          </v-form>
-        </v-card>
-      </v-dialog>
-      <v-snackbar
-        color="red darken-2"
-        v-model="errorLogin"
-        multline
-        timeout="2000"
-        ></v-snackbar
-      >
-    </v-container>
-  </template>
-  <script>
-  import { mapState, mapActions } from "vuex";
-  
-  export default {
-    data: () => ({
-    user: {},
+            </div>
+          </v-col>
+        </v-form>
+      </v-card>
+    </v-dialog>
+    <v-snackbar
+      color="red darken-2"
+      v-model="errorLogin"
+      multline
+      timeout="2000"
+    ></v-snackbar>
+  </v-container>
+</template>
+<script>
+import { mapState, mapActions } from "vuex";
+import axios from "axios";
+
+export default {
+  created() {
+    this.getComputers();
+  },
+  data: () => ({
     show: false,
     valid: true,
     loginPopUp: false,
     errorLogin: false,
     errorMessage: null,
-    }),
-    computed: {
+  }),
+  computed: {
+    ...mapState("auth", ["user"]),
     ...mapState("computer", ["computer", "allComputers"]),
   },
   methods: {
     ...mapActions("computer", [
+      "getComputers",
       "postComputer",
       "editComputer",
       "deleteComputer",
     ]),
 
+    uploadFile() {
+      this.image = this.$refs.file[Object.keys(this.$refs.file)[95]];
+    },
+    async submitFile() {
+      const formData = new FormData();
+      formData.append("file", this.image);
+      const headers = { "Content-Type": "multipart/form-data" };
+      const { data } = await axios.post(
+        `${axios.defaults.baseURL}api/media/images/`,
+        formData,
+        { headers }
+      );
+      return data.attachment_key;
+    },
     async postComputerInfo() {
       try {
-        this.computer.published_by = this.user.pk;
+        this.computer.created_by = this.user.pk;
+        this.computer.computer_photo_attachment_key = await this.submitFile();
         await this.postComputer();
       } catch (e) {
         console.log(e);
@@ -113,6 +141,5 @@
       }
     },
   },
-  };
-  </script>
-  
+};
+</script>
--- src/pages/Computadores.vue
+++ src/pages/Computadores.vue
@@ -8,7 +8,6 @@
         extended
         extension-height="100"
         elevation="1"
-        
       >
         <v-img
           id="Computador"
@@ -20,13 +19,12 @@
         <v-spacer></v-spacer>
         <v-toolbar-title class="title ml-5">
           <h1>Computadores</h1>
-          
         </v-toolbar-title>
-        <v-spacer />       
+        <v-spacer />
       </v-toolbar>
     </v-card>
     <v-container class="computerBody">
-      <CadastrarPC class="cadastro"/>
+      <CadastrarPC class="cadastro" />
       <!-- <v-img class="imgFilter" 
       :src="ComputadorB"
       width="20vw"
@@ -34,54 +32,68 @@
 
       </v-img>
       <h1 class="pl-10" style="color:#3B7994"> Não existem computadores cadastrados. </h1> -->
+      <section v-for="computador in allComputers" :key="computador.id">
         <v-card class="computerCard">
-        <v-item-list>
-          <v-item>
-            <div class="camOne">
-              <h2 style="color:white;">Computador 1</h2>
-            </div>
-          </v-item>
-          <v-item>
-          <div class="camBall">
-            <div class="ball">
-              
-            </div>
-          </div>
-        </v-item>
-          <v-item>
-            <div class="camTwo">
-              <div class="informations">
-                Status : Em uso
+          <v-item-list>
+            <v-item>
+              <div class="camOne">
+                <h2 style="color: white">{{ computador.name }}</h2>
               </div>
-              <div class="informations">
-                Projetos : 1 e 2
+            </v-item>
+            <v-item>
+              <div class="camBall">
+                <div>
+                  <v-avatar class="ball" size="13vh">
+                    <img
+                      alt="Foto do computador"
+                      :src="computador.computer_photo.file"
+                    />
+                  </v-avatar>
+                </div>
               </div>
-              <div class="informations">
-                Coluna : 1
+            </v-item>
+            <v-item>
+              <div class="camTwo">
+                <div class="informations">Status : Em uso</div>
+                <div class="informations">Projetos : 1 e 2</div>
+                <div class="informations">
+                  Fileira : {{ computador.locationX }}
+                </div>
+                <div class="informations">
+                  Coluna : {{ computador.locationY }}
+                </div>
               </div>
-              <div class="informations">
-                Fileira : 3
-              </div>
-            </div>
-          </v-item>
-        </v-item-list>
-      </v-card>
+            </v-item>
+          </v-item-list>
+        </v-card>
+      </section>
     </v-container>
   </v-app>
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
 import Computador from "../assets/computador.png";
 import ComputadorB from "../assets/computadorblue.png";
-import CadastrarPC from "../components/Computador.vue"
+import CadastrarPC from "../components/Computador.vue";
 
 export default {
+  created() {
+    this.getComputers();
+    console.log(this.allComputers);
+  },
   data: () => ({
     Computador,
     ComputadorB,
   }),
-  components:{
-    CadastrarPC
+  components: {
+    CadastrarPC,
+  },
+  computed: {
+    ...mapState("computer", ["allComputers"]),
+  },
+  methods: {
+    ...mapActions("computer", ["getComputers"]),
   },
 };
 </script>
@@ -98,17 +110,17 @@ export default {
   text-align: center;
   align-content: center;
 }
-.computerBody{
+.computerBody {
   max-width: 70vw;
   height: 100vh;
   margin-top: 2vh;
   justify-content: center;
   overflow-wrap: break-word;
 }
-.imgFilter{
+.imgFilter {
   filter: opacity(0.2);
 }
-.computerCard{
+.computerCard {
   display: flex;
   height: 40vh;
   width: 20vw;
@@ -118,57 +130,53 @@ export default {
   justify-content: center;
   align-items: center;
 }
-.titleComputer{
+.titleComputer {
   display: flex;
   justify-content: center;
-   height: 10vh;
-   width: auto;
-
-}
-.camOne{
-  background-color: #3B7994;
   height: 10vh;
-  width:20vw;
+  width: auto;
+}
+.camOne {
+  background-color: #3b7994;
+  height: 10vh;
+  width: 20vw;
   border-radius: 10px 10px 0px 0px;
   display: flex;
   justify-content: center;
   align-items: center;
-
 }
-.camTwo{
+.camTwo {
   background-color: #ffffff25;
   height: 20vh;
-  width:20vw;
+  width: 20vw;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   border-radius: 10px;
 }
-.informations{
-  color: #3B7994;
+.informations {
+  color: #3b7994;
   padding-top: 2vh;
 }
-.ball{
+.ball {
   height: 13vh;
   width: 13vh;
   background-color: rgb(255, 255, 255);
-  background-image:url(../assets/computadorblue2.png);
-  background-size:10vh;
-  background-repeat:no-repeat;
+  background-size: 10vh;
+  background-repeat: no-repeat;
   border-radius: 100%;
-  background-position:center;
-  border:1px solid #3B7994;
+  background-position: center;
+  border: 1px solid #3b7994;
 }
-.camBall{
+.camBall {
   width: 20vw;
   height: 10vh;
   display: flex;
   align-items: center;
   justify-content: center;
-  
 }
-.cadastro{
+.cadastro {
   margin-top: 6vh;
   width: auto;
   height: 10vh;
